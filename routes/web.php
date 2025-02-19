8ea21eb696cad6972fce338104369c77419580b2
Bintx <tanpojeneng77@gmail.com>
2025-02-19 10:32:03 +0700

    membuat databse, migration, tambah controller dan membuat tampilan dasar

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\JalanController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::resource('jalan', JalanController::class);
