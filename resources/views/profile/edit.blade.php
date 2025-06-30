f0b51197580b203d872b998d3fb6ae79bd015335
Bintx <tanpojeneng77@gmail.com>
2025-06-30 18:41:26 +0700

    tambah my profile

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,29 +1,49 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Profile') }}
-        </h2>
-    </x-slot>
+{{-- resources/views/profile/edit.blade.php --}}
+@extends('layouts.app')
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
+@section('title', 'Profil Pengguna')
+
+@section('content')
+    <div class="d-flex flex-wrap align-items-center justify-content-between gap-3 mb-24">
+        <h6 class="fw-semibold mb-0">Profil Pengguna</h6>
+        <ul class="d-flex align-items-center gap-2">
+            <li class="fw-medium">
+                <a href="{{ route('dashboard') }}" class="d-flex align-items-center gap-1 hover-text-primary">
+                    <iconify-icon icon="solar:home-smile-angle-outline" class="icon text-lg"></iconify-icon>
+                    Dashboard
+                </a>
+            </li>
+            <li>-</li>
+            <li class="fw-medium">Profil</li>
+        </ul>
+    </div>
+
+    <div class="row row-cols-1 gy-4">
+        {{-- Form Informasi Profil --}}
+        <div class="col-lg-6">
+            <div class="card h-100">
+                <div class="card-body p-24">
                     @include('profile.partials.update-profile-information-form')
                 </div>
             </div>
+        </div>
 
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
+        {{-- Form Update Password --}}
+        <div class="col-lg-6">
+            <div class="card h-100">
+                <div class="card-body p-24">
                     @include('profile.partials.update-password-form')
                 </div>
             </div>
+        </div>
 
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
+        {{-- Form Hapus Akun --}}
+        <div class="col-lg-12">
+            <div class="card h-100">
+                <div class="card-body p-24">
                     @include('profile.partials.delete-user-form')
                 </div>
             </div>
         </div>
     </div>
-</x-app-layout>
+@endsection
